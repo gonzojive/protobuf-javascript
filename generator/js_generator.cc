964e81768fe89d609d4e7d29e4b17c3d0fe35114
Dave Masselink <masselink@google.com>
2023-06-28 23:29:36 +0000

    attempting to correctly build JS output

--- generator/js_generator.cc
+++ generator/js_generator.cc
@@ -3672,9 +3672,12 @@ void Generator::GenerateExtension(const GeneratorOptions& options,
            : GetNamespace(options, field->file()));
 
   const std::string extension_object_name = JSObjectFieldName(options, field);
-  GenerateClass(options, type_names, printer, nullptr);
-  GenerateClassRegistration(options, type_names, printer, nullptr);
-  GenerateClassFields(options, type_names, printer, nullptr);
+
+  const Descriptor* desc = field->file()->message_type(0);
+
+  GenerateClass(options, type_names, printer, desc);
+  GenerateClassRegistration(options, type_names, printer, desc);
+  GenerateClassFields(options, type_names, printer, desc);
   printer->Print(
       "\n"
       //"class ExtendedClass extends $class$ {}\n"
