2815f82932b7761c997de2b43855b3c3ea7bab16
Dave Masselink <masselink@google.com>
2023-06-29 19:42:32 +0000

    attempting to correctly build JS output

--- generator/js_generator.cc
+++ generator/js_generator.cc
@@ -3689,7 +3689,7 @@ void Generator::GenerateExtension(const GeneratorOptions& options,
       " * @type {!jspb.ExtensionFieldInfo<$extensionType$>}\n"
       " */\n"
       ""
-      "ExtendedClass.$name$ = new jspb.ExtensionFieldInfo(\n",
+      "$class$.$name$ = new jspb.ExtensionFieldInfo(\n",
       "nameInComment", extension_object_name, "name", extension_object_name,
       "class", extension_scope, "extensionType", 
       JSFieldTypeAnnotation(options, field,
