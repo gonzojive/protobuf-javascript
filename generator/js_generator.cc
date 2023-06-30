973148624659d67d8ef68a09792fba6a8037572b
Dave Masselink <masselink@google.com>
2023-06-30 17:18:38 +0000

    attempting to correctly build JS output

--- generator/js_generator.cc
+++ generator/js_generator.cc
@@ -3669,7 +3669,8 @@ void Generator::GenerateExtension(const GeneratorOptions& options,
                                   const FieldDescriptor* field) const {
   //std::string extension_scope = ImportAliases(TypeNames::Es6TypeNames(options, field->file()), field->file());//GetNamespace(options, field->file());
   //std::string extension_scope = TypeNames::JsName(field->full_name());
-  std::string extension_scope = type_names.JsExpression(field->message_type());  
+  //std::string extension_scope = type_names.JsExpression(field->message_type());
+  std::string extension_scope = TypeNames::JsExpression(field-full_name());  
       //ImportAliases(type_names, *file->dependency(i))
       // (field->extension_scope()
       //      ? field->extension_scope()->name()
